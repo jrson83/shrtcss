c2ed7ad8b35c7b4aaa910df206c2c57a89f971ba
Jrson <45284565+jrson83@users.noreply.github.com>
2023-07-19 03:35:44 +0200

    feat: add heading

--- playground/preact-ts/src/components/ui/index.ts
+++ playground/preact-ts/src/components/ui/index.ts
@@ -1,1 +1,2 @@
 export { default as Divider } from './divider'
+export { default as Heading } from './heading'
--- playground/react-ts/src/App.tsx
+++ playground/react-ts/src/App.tsx
@@ -1,9 +1,28 @@
-import { Divider } from '#/components'
+import { Divider, Heading } from '#/components'
 
-const App: React.FC = () => {
+type AppType = React.FC
+
+const App: AppType = () => {
   return (
     <div>
-      <p>App</p>
+      <Heading level={'h1'}>
+        Heading <small>h1</small>
+      </Heading>
+      <Heading level={'h2'}>
+        Heading <small>h2</small>
+      </Heading>
+      <Heading level={'h3'}>
+        Heading <small>h3</small>
+      </Heading>
+      <Heading level={'h4'}>
+        Heading <small>h4</small>
+      </Heading>
+      <Heading level={'h5'}>
+        Heading <small>h5</small>
+      </Heading>
+      <Heading level={'h6'}>
+        Heading <small>h6</small>
+      </Heading>
       <Divider />
     </div>
   )
