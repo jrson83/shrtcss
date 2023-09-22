a1441576420d63fa9876728684a6a994d31b13c8
Jrson <45284565+jrson83@users.noreply.github.com>
2023-09-22 20:43:45 +0200

    test(react): add table test

---
+++ packages/react/src/table/table.spec.tsx
@@ -0,0 +1,26 @@
+import '@testing-library/jest-dom/vitest'
+import { cleanup, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, test } from 'vitest'
+import { type User, users } from './setup.data'
+import Table from './table'
+
+describe('Table component test', () => {
+  afterEach(cleanup)
+
+  test('Should render default table', () => {
+    render(
+      <Table<User, keyof User>
+        items={users}
+        columns={[
+          { key: 'username', header: 'Username' },
+          { key: 'address', header: 'Address' },
+          { key: 'role', header: 'Role' },
+        ]}
+        caption="Userlist"
+      />
+    )
+
+    const table = screen.getByRole('table')
+    expect(table).toBeInTheDocument()
+  })
+})
