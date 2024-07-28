1337fd0f26a479fe4550a2ed7d7601dd205fb552
jrson83 <me@jrson.de>
2024-07-28 02:05:04 +0200

    chore: update workspace

---
+++ .scripts/changelogs.mjs
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+import {
+  parseMonorepoConventionalCommits,
+  renderMonorepoConventionalCommits,
+} from '@bscotch/workspaces'
+
+/**
+ * CommitType `chore` is disabled by default for all packages
+ * and should be used for changelog (and other) related commits
+ *
+ * { pattern: /^chore?$/, group: 'Chores' },
+ */
+const parsed = await parseMonorepoConventionalCommits('.', {
+  types: [
+    { pattern: /^feat?$/, group: 'Features' },
+    { pattern: /^fix?$/, group: 'Bug Fixes' },
+    { pattern: /^docs?$/, group: 'Documentation' },
+    { pattern: /^style?$/, group: 'Styles' },
+    { pattern: /^refactor?$/, group: 'Code Refactoring' },
+    { pattern: /^perf?$/, group: 'Performance Improvements' },
+    { pattern: /^test?$/, group: 'Tests' },
+    { pattern: /^build?$/, group: 'Builds' },
+    { pattern: /^revert?$/, group: 'Reverts' },
+  ],
+})
+
+const sortOrder = [
+  'Features',
+  'Code Refactoring',
+  'Bug Fixes',
+  'Reverts',
+  'Builds',
+  'Performance Improvements',
+  'Styles',
+  'Documentation',
+  'Tests',
+]
+
+await renderMonorepoConventionalCommits(
+  parsed,
+  (project, versions) => {
+    if (project.isRoot || project.package.name.endsWith('docs')) return
+    const title = `# Changelog - ${project.package.name}`
+    const versionStrings = versions.map((version) => {
+      const header = `## ${version.version} (${
+        version.date.toISOString().split('T')[0]
+      })`
+      const groups = Object.keys(version.groups).sort(
+        (a, b) => sortOrder.indexOf(a) - sortOrder.indexOf(b)
+      )
+      const sections = groups.map((group) => {
+        const changes = version.groups[group]
+        const commits = changes
+          ?.map((commit) => {
+            const shortHash = commit.log.hash.slice(0, 7)
+            return `- ${commit.variables.description} ([${shortHash}](https://github.com/jrson83/shrtcss/commit/${shortHash}))`
+          })
+          .join('\n')
+        return `### ${group}\n\n${commits}`
+      })
+      return `${header}${
+        sections.length !== 0 ? `\n\n${sections.join('\n\n')}` : `\n`
+      }`
+    })
+    return `${title}\n\n${versionStrings.join('\n\n')}\n`
+  },
+  { filename: 'CHANGELOG.md' }
+)
